4c425bbd735a0080e8efd6c277048b7bd84b1b91
michellibrito <michellidibrito@gmail.com>
2023-01-17 18:38:35 -0300

    update spring boot

--- src/main/java/com/ms/email/services/EmailService.java
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -10,6 +10,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -24,6 +25,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
+    @Transactional
     public EmailModel sendEmail(EmailModel emailModel) {
         emailModel.setSendDateEmail(LocalDateTime.now());
         try{
