3164cf32808145815c5eb369d922d39219200e8d
Marcelo Henrique Ribeiro <mghenr@gmail.com>
2023-03-06 16:12:15 -0300

    feat criada entidade endereco

---
+++ src/main/java/com/pods/fclabs/models/Endereco.java
@@ -0,0 +1,40 @@
+package com.pods.fclabs.models;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+@ApiModel(value = "Objeto Entereço", subTypes = {Endereco.class})
+@Data
+@Entity
+@Table(name = "TB_ENDERECO")
+public class Endereco implements Serializable {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @ApiModelProperty(hidden = true)
+    private UUID id;
+
+    @ApiModelProperty(dataType = "String", example = "Cep do usuario no formato xx.xxx-xxx ou xxxxxxxx", required = true, position = 1)
+    private String cep;
+    @ApiModelProperty(dataType = "String", example = "Nome do logadouro", required = true, position = 2)
+    private String logadouro;
+    @ApiModelProperty(dataType = "String", example = "Tipo de logadouro", required = true, position = 3)
+    private Morada morada;
+    @ApiModelProperty(dataType = "String", example = "Numero da casa", required = true, position = 4)
+    private String numero;
+    @ApiModelProperty(dataType = "String", example = "Complemento", required = false, position = 5)
+    private String complemento;
+    @ApiModelProperty(dataType = "String", example = "Bairro do usuario", required = true, position = 6)
+    private String bairro;
+    @ApiModelProperty(dataType = "String", example = "Cidade do usuario", required = true, position = 7)
+    private String cidade;
+    @ApiModelProperty(hidden = true)
+    private Date dtCriacao;
+    @ApiModelProperty(hidden = true)
+    private Date dtUltAlteracao;
+}
---
+++ src/main/java/com/pods/fclabs/models/Morada.java
@@ -0,0 +1,5 @@
+package com.pods.fclabs.models;
+
+public enum Morada {
+    RUA, AVENIDA, VILA, TRAVESSA, ACESSO, SITIO, RODOVIA, TREVO, VIELA, PORTO, VIA, RECANTO
+}
